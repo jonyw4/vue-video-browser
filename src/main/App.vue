d2e5030e4869fc8fe026866fc56750fe20aa541b
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-06-10 15:15:15 -0300

    refactor: update app vue

--- src/main/App.vue
+++ src/main/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <SearchBar @onInput="onInputSearchBar"/>
-    <article v-for="video in videoList" :key="video.id">
-      {{video.title}}
-    </article>
+    <VideoList>
+      <VideoListItem v-for="video in videoList" :key="video.id" :video="video" />
+    </VideoList>
   </div>
 </template>
 
 <script>
-import SearchBar from '../components/SearchBar.vue'
+import { SearchBar, VideoList, VideoListItem } from '../components'
 import { createVideoBrowserService } from './factories'
 
 const videoBrowserService = createVideoBrowserService()
 
 export default {
-  components: { SearchBar },
+  components: { SearchBar, VideoList, VideoListItem },
   name: 'App',
   data: function() {
     return {
