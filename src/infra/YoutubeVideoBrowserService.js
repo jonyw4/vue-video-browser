1d8208d29d0c1cf8044f4c547438830edda811a6
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-06-10 15:14:25 -0300

    chore: remove log youtube data api key

--- src/infra/YoutubeVideoBrowserService.js
+++ src/infra/YoutubeVideoBrowserService.js
@@ -5,7 +5,6 @@ export class YoutubeVideoBrowserService {
     this.httpClient = httpClient;
   }
   async search(query) {
-    console.log(YOUTUBE_DATA_API_KEY);
     const response = await this.httpClient.get("search", {
       key: YOUTUBE_DATA_API_KEY,
       type: "video",
