392e8ae8689c448e949fb768e114598d6ed5fb57
Jonathan (Jony) Célio <jonathan.silva@gamersclub.com.br>
2021-06-10 15:14:55 -0300

    feat: add video list and list item components

---
+++ src/components/VideoList.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>
+    <h2>Video List</h2>
+    <ul>
+      <slot></slot>
+    </ul>
+  </section>
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as SearchBar } from './SearchBar.vue';
+export { default as VideoList } from "./VideoList.vue";
+export { default as VideoListItem } from './VideoListItem.vue';
