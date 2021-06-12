ed0c57aea9f70f64aa948eb8662a1352a0abdc49
Jonathan Célio <jonathan.clio@hotmail.com>
2021-06-12 19:31:33 -0300

    feat: create video player component

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <div class="ratio ratio-16x9">
+      <iframe  
+        type="text/html"
+        :src="url"
+        frameborder="0"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VideoPlayer',
+  props: ['title', 'description', 'url']
+}
+</script>
